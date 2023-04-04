52b972e07d730a6c2d2a3ea5f267c6d2dcc9d7bd
Alexndrs <alexandred56700@gmail.com>
2023-04-04 16:39:17 +0200

    update projet en matlab

--- transcription en matlab/transcription.m
+++ transcription en matlab/transcription.m
@@ -1,6 +1,6 @@
 %% LECTURE DU FICHIER AUDIO
 
-[y,Fe] = audioread('audio.wav');
+[y,Fe] = audioread('do re mi fa so la.wav');
 
 %y -> tableau des valeur du signal normalisés entre -1 et 1
 %Fs -> fréquence d'échantillonage
@@ -10,9 +10,9 @@ L = sz(1); %L -> nombre de valeurs échantillonés
 
 tab_t = [0:(1/Fe):((L-1)/Fe)]; %t -> tableau des temps d'échantillonage
 
-t_gap = 0.5;
+t_gap = 0.2;
 for k = 1:L/1000
-    t = tab_t(1000*k)
+    t = tab_t(1000*k);
     t1 = t - t_gap/2;
     t2 = t + t_gap/2;
     y_res = fenetrage_hamming(y,Fe,t1,t2);
@@ -24,7 +24,7 @@ for k = 1:L/1000
     X = fft(y_res);
 
     %spectre sans le repliement (signal restreint de 0 à Fs/2)
-    TF = X(1:L/2+1);  
+    TF = X(1:floor(L/2)+1);  
     tab_f = Fe*(0:(L/2))/L;
     plot(tab_f,abs(TF));
 
@@ -32,7 +32,7 @@ for k = 1:L/1000
     H = 5;
     seuil = 30;
     f_res = fmax(TF, H, tab_f, seuil)
-    title(['freq prépondérante du spectre : ',num2str(f_res)]);
+    title(['f : ',num2str(f_res),'/// t : ',num2str(t)]);
     xlim([0 1000])
     ylim([0 2000])
     pause(0.01);

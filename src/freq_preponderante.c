dc4362b5d5890a3bad5559fbf152692d1f06650b
tadeuszpm <tadeusz.plague@ensta-paris.fr>
2023-04-18 16:24:38 +0200

    Filtre coupe bande Création de la fonction filtre coupe bande, prise en compte des erreurs dans freq_preponderante

--- src/freq_preponderante.c
+++ src/freq_preponderante.c
@@ -6,7 +6,7 @@
 
 float frequence_preponderante(double* tab_amplitude,float* tab_frequence,float seuil, int H, int len_tab)
 {
-    float fprep;
+    float fprep = -1;
     float amp_max = seuil;
     float f;
     float prod_spec;
@@ -14,7 +14,7 @@ float frequence_preponderante(double* tab_amplitude,float* tab_frequence,float s
     {
         f = tab_frequence[k];
         prod_spec = 1;
-        for (int i = 1; i <= H; i++)
+        for (int i = 1; i <= H; i++)        // On indice de 1 à H compris pour prendre en compte le fondamental
         {
             double r = REAL(tab_amplitude,i*k);
             double im = IMAG(tab_amplitude,i*k);
@@ -26,5 +26,6 @@ float frequence_preponderante(double* tab_amplitude,float* tab_frequence,float s
             fprep = f;
         }
     }
+    if (fprep == -1){printf("Il ne s'est rien passé\n");}
     return (fprep); 
 }
